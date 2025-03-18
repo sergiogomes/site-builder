9831031cd9ba70b81007c50bb78d1610d10b22f6
sergiogomes <sergiopgomes92@gmail.com>
2025-03-17 22:22:20 -0300

    - update jest to support sass

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
+  }
 }
